90c0a983d7e8f9c1f93c8c1eaa736dc84032fd46
jaybon1 <ponggas2@gmail.com>
2023-06-13 10:33:38 +0900

    컴퍼지션 완료

--- src/ex05/composit/BigBurger.java
+++ src/ex05/composit/BigBurger.java
@@ -7,6 +7,11 @@ public class BigBurger extends Burger {
         System.out.println("빅버거가 만들어졌습니다");
     }
 
+    public BigBurger(int price) {
+        super(price, "빅버거");
+        System.out.println("빅버거가 만들어졌습니다");
+    }
+
     public BigBurger(int price, String desc) {
         super(price, desc);
         System.out.println("빅버거가 만들어졌습니다");
